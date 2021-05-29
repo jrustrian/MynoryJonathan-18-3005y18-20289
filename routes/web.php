7acf8bf1d1c6b419f218eea94489c5604e90f3a6
jonathan hernandez <jrustrianh@miumg.edu.gt>
2021-05-28 22:45:54 -0500

    creacion de contacto y insertar contactos

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactUsFormController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('/contact');
+});
+Route::get('/contact', [ContactUsFormController::class, 'createForm']);
+
+Route::post('/contactsave', [ContactUsFormController::class, 'ContactUsForm'])->name('contact.store');
